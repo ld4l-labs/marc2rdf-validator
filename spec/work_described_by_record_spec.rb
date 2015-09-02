120a7cebe2ae9b4d8c0ea6018ed2c73700bd1563
Naomi Dushay <ndushay@stanford.edu>
2015-09-02 11:37:45 -0700

    DRYing up work_described_by_record_spec

--- spec/work_described_by_record_spec.rb
+++ spec/work_described_by_record_spec.rb
@@ -16,22 +16,32 @@ describe 'the work described by the MARC record' do
   # 130|240  # uniform title
   # 880 for 130|240  vernacular uniform title
 
+  let(:marc_ldr_001_008) {
+    '<record xmlns="http://www.loc.gov/MARC21/slim">
+      <leader>01855cemaa22003131a 4500</leader>
+      <controlfield tag="001">aRECORD_ID</controlfield>
+      <controlfield tag="008">760219s1925    en            000 0 eng  </controlfield>'
+  }
+  let(:work_squery) {
+    SPARQL.parse("PREFIX bf: <http://bibframe.org/vocab/>
+                  SELECT DISTINCT ?work
+                  WHERE {
+                    ?work a bf:Work .
+                  }") }
+  let(:work_title_sparql) {"PREFIX bf: <http://bibframe.org/vocab/>
+                            SELECT DISTINCT ?work
+                            WHERE {
+                              ?work a bf:Work .
+                              ?work bf:authorizedAccessPoint ?aap .
+                              FILTER regex(?aap, 'TITLE_REGEX', 'i')
+                            }" }
+
   context "130" do
-    let(:beowulf_work_squery) {
-      SPARQL.parse("PREFIX bf: <http://bibframe.org/vocab/>
-                    SELECT DISTINCT ?work
-                    WHERE {
-                      ?work a bf:Work .
-                      ?work bf:authorizedAccessPoint ?aap .
-                      FILTER regex(?aap, 'Beowulf', 'i')
-                    }") }
+    let(:beowulf_work_title_squery) { SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'Beowulf'))}
 
     it '130 ‡a only has single work' do
-      marcxml_str = '<record xmlns="http://www.loc.gov/MARC21/slim">
-        <leader>01855cemaa22003131a 4500</leader>
-        <controlfield tag="001">a130a_only</controlfield>
-        <controlfield tag="008">760219s1925    en            000 0 eng  </controlfield>
-        <datafield ind1="0" ind2=" " tag="130">
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', '130a_only') +
+        '<datafield ind1="0" ind2=" " tag="130">
           <subfield code="a">Beowulf.</subfield>
         </datafield>
         <datafield ind1="0" ind2="0" tag="245">
@@ -39,31 +49,40 @@ describe 'the work described by the MARC record' do
         </datafield>
       </record>'
       g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '130a_only')
-      expect(g.query(beowulf_work_squery).size).to eq 1
+      expect(g.query(work_squery).size).to eq 1
+      expect(g.query(beowulf_work_title_squery).size).to eq 1
     end
-    it '130 ‡a, ‡=  has single work' do
-      marcxml_str = '<record xmlns="http://www.loc.gov/MARC21/slim">
-        <leader>01855cemaa22003131a 4500</leader>
-        <controlfield tag="001">a130a_equal</controlfield>
-        <controlfield tag="008">760219s1925    en            000 0 eng  </controlfield>
-        <datafield ind1="0" ind2=" " tag="130">
+    it '130 ‡a only has single work with title from 130' do
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', '130a_title') +
+        '<datafield ind1="0" ind2=" " tag="130">
+          <subfield code="a">Beowulf.</subfield>
+        </datafield>
+        <datafield ind1="0" ind2="0" tag="245">
+          <subfield code="a">ignore me</subfield>
+        </datafield>
+      </record>'
+      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '130a_title')
+      expect(g.query(work_squery).size).to eq 1
+      expect(g.query(beowulf_work_title_squery).size).to eq 1
+    end
+    it '130 ‡a, ‡= has single work' do
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', '130a_equal') +
+        '<datafield ind1="0" ind2=" " tag="130">
           <subfield code="a">Beowulf.</subfield>
           <subfield code="=">^A944917</subfield>
         </datafield>
         <datafield ind1="0" ind2="0" tag="245">
-          <subfield code="a">Beowulf</subfield>
+          <subfield code="a">ignore me</subfield>
         </datafield>
       </record>'
       g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '130a_equal')
+      expect(g.query(work_squery).size).to eq 1
       # TODO:  would like to keep ^A944917 out of aap ??
-      expect(g.query(beowulf_work_squery).size).to eq 1
+      expect(g.query(beowulf_work_title_squery).size).to eq 1
     end
-    it '130 ‡a, ‡n, ‡?  has single work' do
-      marcxml_str = '<record xmlns="http://www.loc.gov/MARC21/slim">
-        <leader>01855cemaa22003131a 4500</leader>
-        <controlfield tag="001">a130an_question_mark</controlfield>
-        <controlfield tag="008">760219s1925    en            000 0 eng  </controlfield>
-        <datafield ind1="0" ind2=" " tag="130">
+    it '130 ‡a, ‡n, ‡? has single work' do
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', '130an_question_mark') +
+        '<datafield ind1="0" ind2=" " tag="130">
           <subfield code="a">Annale.</subfield>
           <subfield code="n">Reeks B (University of Stellenbosch : 1979)</subfield>
           <subfield code="?">UNAUTHORIZED</subfield>
@@ -75,18 +94,14 @@ describe 'the work described by the MARC record' do
         </datafield>
       </record>'
       g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '130an_question_mark')
-      squery = SPARQL.parse(" PREFIX bf: <http://bibframe.org/vocab/>
-                              SELECT DISTINCT ?work
-                              WHERE {
-                                ?work a bf:Work .
-                                ?work bf:authorizedAccessPoint ?aap .
-                                FILTER regex(?aap, 'Annale', 'i') }")
+      expect(g.query(work_squery).size).to eq 1
       # TODO:  would like to keep UNAUTHORIZED out of aap ??
-      expect(g.query(squery).size).to eq 1
+      work_title_squery = SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'Annale'))
+      expect(g.query(work_title_squery).size).to eq 1
     end
   end # context 130
 
-  context "1xx and 240" do
+  context "100|110|111 and 240" do
     it '100 and 240' do
       fail 'test to be implemented'
     end
@@ -98,7 +113,7 @@ describe 'the work described by the MARC record' do
     end
   end # context 1xx and 240
 
-  context "1xx and 245" do
+  context "100|110|111 and 245" do
     it '100 and 245' do
       fail 'test to be implemented'
     end
