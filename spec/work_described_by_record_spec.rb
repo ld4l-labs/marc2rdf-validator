89bcdae1c3aa011734774b626df632a103b41895
Naomi Dushay <ndushay@stanford.edu>
2015-09-02 17:43:43 -0700

    work_described_by_record_spec: implement 1xx and 240 tests

--- spec/work_described_by_record_spec.rb
+++ spec/work_described_by_record_spec.rb
@@ -28,6 +28,8 @@ describe 'the work described by the MARC record' do
                   WHERE {
                     ?work a bf:Work .
                   }") }
+  # TODO:  rework title query for workTitle (?)
+  # TODO:  title shouldn't have certain subfields (e.g. ‡=, ‡?)
   let(:work_title_sparql) {"PREFIX bf: <http://bibframe.org/vocab/>
                             SELECT DISTINCT ?work
                             WHERE {
@@ -40,7 +42,8 @@ describe 'the work described by the MARC record' do
     let(:beowulf_work_title_squery) { SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'Beowulf'))}
 
     it '130 ‡a only has single work' do
-      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', '130a_only') +
+      rec_id = '130a_only'
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', rec_id) +
         '<datafield ind1="0" ind2=" " tag="130">
           <subfield code="a">Beowulf.</subfield>
         </datafield>
@@ -48,12 +51,13 @@ describe 'the work described by the MARC record' do
           <subfield code="a">Beowulf</subfield>
         </datafield>
       </record>'
-      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '130a_only')
+      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, rec_id)
       expect(g.query(work_squery).size).to eq 1
       expect(g.query(beowulf_work_title_squery).size).to eq 1
     end
     it '130 ‡a only has single work with title from 130' do
-      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', '130a_title') +
+      rec_id = '130a_title'
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', rec_id) +
         '<datafield ind1="0" ind2=" " tag="130">
           <subfield code="a">Beowulf.</subfield>
         </datafield>
@@ -61,12 +65,15 @@ describe 'the work described by the MARC record' do
           <subfield code="a">ignore me</subfield>
         </datafield>
       </record>'
-      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '130a_title')
+      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, rec_id)
       expect(g.query(work_squery).size).to eq 1
       expect(g.query(beowulf_work_title_squery).size).to eq 1
+      work_title_squery = SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'ignore'))
+      expect(g.query(work_title_squery).size).to eq 0
     end
     it '130 ‡a, ‡= has single work' do
-      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', '130a_equal') +
+      rec_id = '130a_equal'
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', rec_id) +
         '<datafield ind1="0" ind2=" " tag="130">
           <subfield code="a">Beowulf.</subfield>
           <subfield code="=">^A944917</subfield>
@@ -75,13 +82,13 @@ describe 'the work described by the MARC record' do
           <subfield code="a">ignore me</subfield>
         </datafield>
       </record>'
-      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '130a_equal')
+      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, rec_id)
       expect(g.query(work_squery).size).to eq 1
-      # TODO:  would like to keep ^A944917 out of aap ??
       expect(g.query(beowulf_work_title_squery).size).to eq 1
     end
     it '130 ‡a, ‡n, ‡? has single work' do
-      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', '130an_question_mark') +
+      rec_id = '130an_question_mark'
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', rec_id) +
         '<datafield ind1="0" ind2=" " tag="130">
           <subfield code="a">Annale.</subfield>
           <subfield code="n">Reeks B (University of Stellenbosch : 1979)</subfield>
@@ -93,25 +100,111 @@ describe 'the work described by the MARC record' do
           <subfield code="c">Universiteit van Stellebosch.</subfield>
         </datafield>
       </record>'
-      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '130an_question_mark')
+      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, rec_id)
       expect(g.query(work_squery).size).to eq 1
-      # TODO:  would like to keep UNAUTHORIZED out of aap ??
       work_title_squery = SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'Annale'))
       expect(g.query(work_title_squery).size).to eq 1
     end
   end # context 130
 
   context "100|110|111 and 240" do
-    it '100 and 240' do
-      fail 'test to be implemented'
+    it '100 ‡a, ‡? and 240 ‡a, ‡? has single work' do
+      rec_id = '100a_240a_question_marks'
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', rec_id) +
+        '<datafield ind1="1" ind2=" " tag="100">
+          <subfield code="a">Davis, Miles.</subfield>
+          <subfield code="?">UNAUTHORIZED</subfield>
+        </datafield>
+        <datafield ind1="1" ind2="0" tag="240">
+          <subfield code="a">Birth of the cool</subfield>
+          <subfield code="?">UNAUTHORIZED</subfield>
+        </datafield>
+        <datafield ind1="1" ind2="0" tag="245">
+          <subfield code="a">ignored</subfield>
+          <subfield code="c">Miles Davis.</subfield>
+        </datafield>
+      </record>'
+      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, rec_id)
+      expect(g.query(work_squery).size).to eq 1
+      title_squery = SPARQL.parse(" PREFIX bf: <http://bibframe.org/vocab/>
+                                    SELECT DISTINCT ?work
+                                    WHERE {
+                                      ?work a bf:Work .
+                                      ?work bf:authorizedAccessPoint ?aap .
+                                      FILTER regex(?aap, 'Davis, Miles', 'i')
+                                      FILTER regex(?aap, 'Birth of the cool', 'i')
+                                    }")
+      expect(g.query(title_squery).size).to eq 1
+      work_title_squery = SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'ignore'))
+      expect(g.query(work_title_squery).size).to eq 0
     end
-    it '110 and 240' do
-      fail 'test to be implemented'
+    it '110 ‡a, ‡b, ‡= and 240 ‡a has single work' do
+      rec_id = '110ab_240a_equal'
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', rec_id) +
+        '<datafield ind1="1" ind2=" " tag="110">
+          <subfield code="a">South Africa.</subfield>
+          <subfield code="b">Department of Education</subfield>
+          <subfield code="=">^A1298224</subfield>
+        </datafield>
+        <datafield ind1="1" ind2="0" tag="240">
+          <subfield code="a">Annual report (Online)</subfield>
+        </datafield>
+        <datafield ind1="1" ind2="0" tag="245">
+          <subfield code="a">ignore</subfield>
+          <subfield code="h">[electronic resource] /</subfield>
+          <subfield code="c">Department of Education.</subfield>
+        </datafield>
+      </record>'
+      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, rec_id)
+      expect(g.query(work_squery).size).to eq 1
+      title_squery = SPARQL.parse(" PREFIX bf: <http://bibframe.org/vocab/>
+                                    SELECT DISTINCT ?work
+                                    WHERE {
+                                      ?work a bf:Work .
+                                      ?work bf:authorizedAccessPoint ?aap .
+                                      FILTER regex(?aap, 'South Africa', 'i')
+                                      FILTER regex(?aap, 'Department of Education', 'i')
+                                      FILTER regex(?aap, 'Annual report', 'i')
+                                    }")
+      expect(g.query(title_squery).size).to eq 1
+      work_title_squery = SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'ignore'))
+      expect(g.query(work_title_squery).size).to eq 0
+      work_title_squery = SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'electronic resource'))
+      expect(g.query(work_title_squery).size).to eq 0
     end
-    it '111 and 240' do
-      fail 'test to be implemented'
+    it '111 ‡a, ‡? and 240 ‡a has single work' do
+      rec_id = '111a_240a_equal'
+      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', rec_id) +
+        '<datafield ind1="2" ind2=" " tag="111">
+          <subfield code="a">International Workshop on Faces.</subfield>
+          <subfield code="?">UNAUTHORIZED</subfield>
+        </datafield>
+        <datafield ind1="1" ind2="0" tag="240">
+          <subfield code="a">Proceedings (Online)</subfield>
+        </datafield>
+        <datafield ind1="1" ind2="0" tag="245">
+          <subfield code="a">ignore a</subfield>
+          <subfield code="h">[ignore h] /</subfield>
+          <subfield code="c">ignore c</subfield>
+        </datafield>
+      </record>'
+      g = self.send(MARC2BF_GRAPH_METHOD, marcxml_str, rec_id)
+      expect(g.query(work_squery).size).to eq 1
+      title_squery = SPARQL.parse(" PREFIX bf: <http://bibframe.org/vocab/>
+                                    SELECT DISTINCT ?work
+                                    WHERE {
+                                      ?work a bf:Work .
+                                      ?work bf:authorizedAccessPoint ?aap .
+                                      FILTER regex(?aap, 'International Workshop on Faces', 'i')
+                                      FILTER regex(?aap, 'Proceedings', 'i')
+                                    }")
+      expect(g.query(title_squery).size).to eq 1
+      work_title_squery = SPARQL.parse(work_title_sparql.sub('TITLE_REGEX', 'ignore'))
+      expect(g.query(work_title_squery).size).to eq 0
     end
   end # context 1xx and 240
+
+  # TODO:  what if 240 and no 1xx?
 
   context "100|110|111 and 245" do
     it '100 and 245' do
@@ -121,6 +214,7 @@ describe 'the work described by the MARC record' do
       fail 'test to be implemented'
     end
     it '111 and 245' do
+      # 5666387
       fail 'test to be implemented'
     end
   end # context 1xx and 245
