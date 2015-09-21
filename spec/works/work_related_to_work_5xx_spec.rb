af2ce873de2a50645ea0e10942f49587577563a4
Naomi Dushay <ndushay@stanford.edu>
2015-09-21 15:41:46 -0700

    add 533 tests to work_related_work_5xx_spec

--- spec/works/work_related_to_work_5xx_spec.rb
+++ spec/works/work_related_to_work_5xx_spec.rb
@@ -54,9 +54,103 @@ describe 'work related to the work described by the MARC record' do
   end # 525
 
   context "533" do
-    it 'need tests for 533' do
-      fail 'need example data for 533'
-    end
+    context "electronic repro" do
+      let(:g) {
+        marcxml_str =
+          '<record xmlns="http://www.loc.gov/MARC21/slim">
+            <leader>06174nam a2200445Ia 4500</leader>
+            <controlfield tag="001">533works</controlfield>
+            <controlfield tag="008">080813s2008    njua    s     001 0 eng d</controlfield>
+            <datafield ind1="1" ind2="0" tag="245">
+              <subfield code="a">Expert podcasting practices for dummies</subfield>
+              <subfield code="h">[electronic resource] /</subfield>
+              <subfield code="c">by Tee Morris, Evo Terra, and Ryan Williams.</subfield>
+            </datafield>
+            <datafield ind1=" " ind2=" " tag="533">
+              <subfield code="a">Electronic reproduction.</subfield>
+              <subfield code="b">Boston, Mass. :</subfield>
+              <subfield code="c">Safari Books Online,</subfield>
+              <subfield code="d">2008.</subfield>
+              <subfield code="n">Mode of access:  World Wide Web.</subfield>
+              <subfield code="n">Available to subscribing institutions.</subfield>
+              <subfield code="7">s2008    maun s</subfield>
+            </datafield>
+          </record>'
+        self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '533works')
+      }
+      it '2 Works' do
+        expect(g.query(WorkHelpers::WORK_SPARQL_QUERY).size).to eq 2
+      end
+      it 'main Work reproduction of 533 work' do
+          solns = g.query(WorkHelpers::WORK_PROP_WORK_SPARQL_QUERY)
+          expect(solns.size).to eq 1
+          expect(solns.first.prop).to eq RDF::Vocab::Bibframe.reproduction
+      end
+    end # electronic repro
+    context "serial" do
+      let(:g) {
+        marcxml_str =
+          '<record xmlns="http://www.loc.gov/MARC21/slim">
+            <leader>01384cas a22003377a 4500</leader>
+            <controlfield tag="001">533combo_260_300_serial</controlfield>
+            <controlfield tag="008">951215c19uu9999bt ar   b    f0    0eng  </controlfield>
+            <datafield tag="245" ind1="1" ind2="0">
+              <subfield code="a">`Budget estimates for fiscal year ...</subfield>
+              <subfield code="h">[microform] :</subfield>
+              <subfield code="b">Ministry of Health &amp; Education.</subfield>
+            </datafield>
+            <datafield tag="533" ind1=" " ind2=" ">
+              <subfield code="a">Microfiche.</subfield>
+              <subfield code="m">1994/95-</subfield>
+              <subfield code="b">New Delhi :</subfield>
+              <subfield code="c">Library of Congress Office ;</subfield>
+              <subfield code="b">Washington, D.C. :</subfield>
+              <subfield code="c">Library of Congress Photoduplication Service,</subfield>
+              <subfield code="d">1995-</subfield>
+              <subfield code="e">microfiches.</subfield>
+            </datafield>
+          </record>'
+        self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '533combo_260_300_serial')
+      }
+      it '2 Works' do
+        expect(g.query(WorkHelpers::WORK_SPARQL_QUERY).size).to eq 2
+      end
+      it 'main Work reproduction of 533 work' do
+          solns = g.query(WorkHelpers::WORK_PROP_WORK_SPARQL_QUERY)
+          expect(solns.size).to eq 1
+          expect(solns.first.prop).to eq RDF::Vocab::Bibframe.reproduction
+      end
+    end # serial
+    context "recording" do
+      let(:g) {
+        marcxml_str =
+          '<record xmlns="http://www.loc.gov/MARC21/slim">
+            <leader>01779cim a2200373 a 4500</leader>
+            <controlfield tag="001">533combo_260_300_recording</controlfield>
+            <controlfield tag="008">931119s1958    dcunnn         f    eng  </controlfield>
+            <datafield tag="245" ind1="1" ind2="0">
+              <subfield code="a">Eudora Welty reading three of her short stories</subfield>
+              <subfield code="h">[sound recording].</subfield>
+            </datafield>
+            <datafield tag="533" ind1=" " ind2=" ">
+              <subfield code="a">Preservation master.</subfield>
+              <subfield code="b">Washington, D.C. :</subfield>
+              <subfield code="c">Library of Congress Magnetic Recording Laboratory,</subfield>
+              <subfield code="d">1973.</subfield>
+              <subfield code="e">1 sound tape reel : analog, 7 1/2 ips, 2 track, mono. ; 10 in.</subfield>
+            </datafield>
+          </record>'
+        self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '533combo_260_300_recording')
+      }
+      it '2 Works' do
+        expect(g.query(WorkHelpers::WORK_SPARQL_QUERY).size).to eq 2
+      end
+      it 'main Work reproduction of 533 work' do
+          solns = g.query(WorkHelpers::WORK_PROP_WORK_SPARQL_QUERY)
+          expect(solns.size).to eq 1
+          expect(solns.first.prop).to eq RDF::Vocab::Bibframe.reproduction
+      end
+    end # recording
   end # 533
 
   context "534" do
