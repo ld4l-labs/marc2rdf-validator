dfe7264fbd3aef4b88e71fb7f30e4762936660c6
Naomi Dushay <ndushay@stanford.edu>
2015-09-22 17:41:36 -0700

    add placeholder specs for related instances 4xx, 534, 630, 7xx, 8xx

---
+++ spec/instances/instance_from_4xx_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'linkeddata'
+
+describe 'instance related to the instance described by the MARC record' do
+
+# mbshared:related-works calls mbshared:generate-related-instance for any of
+#   (400|410|411|430|440|490...)
+end
