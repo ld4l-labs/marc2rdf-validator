806e017cce8fc0079d39d5bd2e326b2a0308835a
Naomi Dushay <ndushay@stanford.edu>
2015-09-22 17:40:58 -0700

    update instance_related_to_instance_spec

--- spec/instances/instance_related_to_instance_spec.rb
+++ spec/instances/instance_related_to_instance_spec.rb
@@ -6,15 +6,6 @@ describe 'instance related to the instance described by the MARC record' do
 # mbshared:related-works calls mbshared:generate-related-instance for any of
 #   (400|410|411|430|440|490|533|534|630|700|710|711|730|740|760|762|765|767|770|772|773|774|775|777|780|785|787|800|810|811|830)
 
-
-  let(:marc_ldr_001_008) {
-    '<record xmlns="http://www.loc.gov/MARC21/slim">
-      <leader>02308cas a2200541 a 4500</leader>
-      <controlfield tag="001">aRECORD_ID</controlfield>
-      <controlfield tag="008">790906u1921uuuubl ar p   o   0    0por c</controlfield>'
-  }
-
-
 # 760 - Main Series Entry (R) Full | Concise
 # 762 - Subseries Entry (R) Full | Concise
 # 765 - Original Language Entry (R) Full | Concise
@@ -32,11 +23,13 @@ describe 'instance related to the instance described by the MARC record' do
 # 787 - Other Relationship Entry (R) Full | Concise
 
   context "issuedWith" do
-    # /m2b-properties/issuedWith/11331701.ttl
     let(:g) {
-      rec_id = '777'
-      marcxml_str = marc_ldr_001_008.sub('RECORD_ID', rec_id) +
-        '<datafield tag="245" ind1="1" ind2="2">
+      marcxml_str =
+        '<record xmlns="http://www.loc.gov/MARC21/slim">
+          <leader>02308cas a2200541 a 4500</leader>
+          <controlfield tag="001">11331701</controlfield>
+          <controlfield tag="008">790906u1921uuuubl ar p   o   0    0por c</controlfield>
+        <datafield tag="245" ind1="1" ind2="2">
           <subfield code="a">A ordem.</subfield>
         </datafield>
         <datafield tag="260" ind1=" " ind2=" ">
@@ -46,33 +39,57 @@ describe 'instance related to the instance described by the MARC record' do
           <subfield code="t">Boletim do Instituto Cato&#x301;lico de Cooperac&#x327;a&#x303;o Intelectual</subfield>
         </datafield>
       </record>'
-      self.send(MARC2BF_GRAPH_METHOD, marcxml_str, rec_id)
+      self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '11331701')
     }
     it '2 instances' do
       expect(g.query(InstanceHelpers::INSTANCE_SPARQL_QUERY).size).to eq 2
     end
     it '1 instanceOf' do
-      expect(g.query(InstanceHelpers::INSTANCE_OF_SPARQL_QUERY).size).to eq 2
+      expect(g.query(InstanceHelpers::INSTANCE_OF_SPARQL_QUERY).size).to eq 1
     end
     it 'main instance issuedWith 777 instance' do
+      # TODO:  is this the right direction?
       expect_instance2instance_property(g, 1, RDF::Vocab::Bibframe.issuedWith)
     end
   end
 
   context "otherPhysicalFormat" do
-    # /m2b-properties/otherPhysicalFormat/11338843.ttl
-    # /m2b-properties/otherPhysicalFormat/11737193.ttl
-    # /m2b-properties/otherPhysicalFormat/12734030.ttl
-    # and lots more
-  end
-
-  context "relatedInstance" do
-    # /m2b-properties/relatedInstance/3556119.ttl  (or not)
-  end
-
-  context "reproduction" do
-    # m2b-properties/reproduction/723007.ttl
-    # and more
+    let(:g) {
+      marcxml_str =
+        '<record xmlns="http://www.loc.gov/MARC21/slim">
+          <leader>01780cas a2200445 a 4500</leader>
+          <controlfield tag="001">11338843</controlfield>
+          <controlfield tag="008">800417d18941920miumn   a     0   a0eng c</controlfield>
+          <datafield tag="245" ind1="1" ind2="4">
+            <subfield code="a">The Advocate of peace</subfield>
+            <subfield code="h">[microform].</subfield>
+          </datafield>
+          <datafield tag="260" ind1=" " ind2=" ">
+            <subfield code="a">Boston, Mass. :</subfield>
+            <subfield code="b">[American Peace Society],</subfield>
+            <subfield code="c">1894-1920.</subfield>
+          </datafield>
+          <datafield tag="300" ind1=" " ind2=" ">
+            <subfield code="a">27 v. ;</subfield>
+            <subfield code="c">24-31 cm.</subfield>
+          </datafield>
+          <datafield tag="776" ind1="1" ind2=" ">
+            <subfield code="t">Advocate of peace (1894)</subfield>
+            <subfield code="w">(OCoLC)6797409</subfield>
+          </datafield>
+      </record>'
+      self.send(MARC2BF_GRAPH_METHOD, marcxml_str, '11338843')
+    }
+    it '2 instances' do
+      expect(g.query(InstanceHelpers::INSTANCE_SPARQL_QUERY).size).to eq 2
+    end
+    it '1 instanceOf' do
+      expect(g.query(InstanceHelpers::INSTANCE_OF_SPARQL_QUERY).size).to eq 1
+    end
+    it 'main instance issuedWith 776 instance' do
+      # TODO:  is this the right direction?
+      expect_instance2instance_property(g, 1, RDF::Vocab::Bibframe.otherPhysicalFormat)
+    end
   end
 
 end
