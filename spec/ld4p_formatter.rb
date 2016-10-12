f5d7ed4b62e6e24de47cee9af89579547540975d
Joshua Greben <jgreben@stanford.edu>
2016-10-12 08:21:54 -0700

    Create method to memoize rspec console_code

--- spec/ld4p_formatter.rb
+++ spec/ld4p_formatter.rb
@@ -8,23 +8,27 @@ class Ld4pFormatter
     @output = output
   end
 
+  def console_code
+    RSpec::Core::Formatters::ConsoleCodes
+  end
+
   def example_group_started notification
-    @output << RSpec::Core::Formatters::ConsoleCodes.wrap("\n#{RSpec::Core::Formatters::ConsoleCodes.wrap(notification.group.description, :yellow)}:\n", :bold)
+    @output << console_code.wrap("\n#{console_code.wrap(notification.group.description, :yellow)}:\n", :bold)
   end
 
   def example_passed notification # ExampleNotification
-    @output << RSpec::Core::Formatters::ConsoleCodes.wrap("\n\t\u2713\t", :success)
-    @output << "#{RSpec::Core::Formatters::ConsoleCodes.wrap(notification.example.description, :cyan)}"
-    @output << "\t#{RSpec::Core::Formatters::ConsoleCodes.wrap(notification.example.location, :blue)}\n"
+    @output << console_code.wrap("\n\t\u2713\t", :success)
+    @output << "#{console_code.wrap(notification.example.description, :cyan)}"
+    @output << "\t#{console_code.wrap(notification.example.location, :blue)}\n"
   end
 
   def example_failed notification # ExampleNotification
-    @output << RSpec::Core::Formatters::ConsoleCodes.wrap("\n\tFail!\t", :failure)
-    @output << "#{RSpec::Core::Formatters::ConsoleCodes.wrap(notification.example.description, :cyan)}"
-    @output << "\t#{RSpec::Core::Formatters::ConsoleCodes.wrap(notification.example.location, :blue)}\n"
+    @output << console_code.wrap("\n\tFail!\t", :failure)
+    @output << "#{console_code.wrap(notification.example.description, :cyan)}"
+    @output << "\t#{console_code.wrap(notification.example.location, :blue)}\n"
   end
 
   def close notification
-    @output << RSpec::Core::Formatters::ConsoleCodes.wrap("\nTHE END.\n", :yellow)
+    @output << console_code.wrap("\nTHE END.\n", :yellow)
   end
 end
