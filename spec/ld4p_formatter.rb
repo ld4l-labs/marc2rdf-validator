829cc55bd3dc7e14332537826475241a7cb6eb5a
Joshua Greben <jgreben@stanford.edu>
2017-02-08 14:42:42 -0800

    Adding ld4p formatter to project

---
+++ spec/ld4p_formatter.rb
@@ -0,0 +1,44 @@
+require 'rspec/core/formatters/console_codes'
+
+class Ld4pFormatter
+  RSpec::Core::Formatters.register self, :example_group_started, :example_passed,
+                                         :example_failed, :close
+
+  def initialize output
+    @output = output
+  end
+
+  def console_code
+    RSpec::Core::Formatters::ConsoleCodes
+  end
+
+  def example_group_started notification
+    @output << console_code.wrap("\n#{console_code.wrap(notification.group.description, :yellow)}:\n", :bold)
+  end
+
+  def example_passed notification # ExampleNotification
+    @output << console_code.wrap("\n\t\u2713\t", :success)
+    @output << "#{console_code.wrap(notification.example.description, :cyan)}"
+    @output << "\t#{console_code.wrap(notification.example.location, :blue)}\n"
+  end
+
+  def example_failed notification # ExampleNotification
+    @output << console_code.wrap("\n\tFail!\t", :failure)
+    @output << "#{console_code.wrap(notification.example.description, :cyan)}"
+    @output << "\t#{console_code.wrap(notification.example.location, :blue)}\n"
+    @output << "\n\t\t#{console_code.wrap(notification.example.execution_result.exception.message, :failure)}\n"
+  end
+
+  # def dump_failures notification
+  #   @output << notification.failed_examples.map do |example|
+  #     full_description = example.full_description
+  #     location = example.location
+  #     message = example.execution_result.exception.message
+  #     "#{full_description} - #{location} #{message}"
+  #   end.join("\n\n\t")
+  # end
+
+  def close notification
+    @output << console_code.wrap("\nTHE END.\n", :yellow)
+  end
+end
